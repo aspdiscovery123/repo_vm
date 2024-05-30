dff03ad5c6b4a4624d6b8fe43ed22cac6d0c7d27
aspdiscovery123 <48548207+aspdiscovery123@users.noreply.github.com>
2024-05-30 12:43:23 +0530

    Add files via upload

---
+++ ccppmodel.py
@@ -0,0 +1,19 @@
+
+
+import joblib
+from flask import Flask,request
+
+model=joblib.load(r'ccpp_model (2).pkl')
+
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+
+def application():
+    data=request.get_json(force=True)
+    print(data)
+    data=data['info']
+    out=model.predict([data])
+    print(out)
+    return str(out) 
+
+app.run(port=5003)
